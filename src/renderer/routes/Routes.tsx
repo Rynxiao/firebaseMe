bd5594fbdb3295912021d0cb453fbfcb7989778f
rynxiao <yuzhongzi91@sina.com>
2021-11-08 21:59:28 +0800

    feat: move firebase module locally

--- src/renderer/routes/Routes.tsx
+++ src/renderer/routes/Routes.tsx
@@ -1,12 +1,10 @@
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
 import React from 'react';
 import Layout from 'renderer/layout/Layout';
-import Login from 'renderer/layout/Login';
 
 const Routes = () => (
   <Router>
     <Switch>
-      <Route path="/login" component={Login} />
       <Route path="/" component={Layout} />
     </Switch>
   </Router>
